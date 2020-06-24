9aa8c4a9b1abdec10772724d83e971d7107295bb
Paulo Moreira <paulo3011@hotmail.com>
2020-06-24 18:16:01 -0300

    first datalake bucktes

--- s3-sample/s3_data_lake_sample.tf
+++ s3-sample/s3_data_lake_sample.tf
@@ -1,12 +1,33 @@
 resource "aws_s3_bucket" "datalake" {
-  bucket = "m3011"
+  bucket = "moreira-datalake"
   region = "us-east-1"
   acl    = "private"
 }
 
-resource "aws_s3_bucket_object" "folder1" {
+resource "aws_s3_bucket_object" "stage" {
   bucket = "${aws_s3_bucket.datalake.id}"
   acl    = "private"
-  key    = "Folder1/"
+  key    = "stage/"
+  source = ""
+}
+
+resource "aws_s3_bucket_object" "stage-firehosestream" {
+  bucket = "${aws_s3_bucket.datalake.id}"
+  acl    = "private"
+  key    = "stage/firehosestream/"
+  source = ""
+}
+
+resource "aws_s3_bucket_object" "raw" {
+  bucket = "${aws_s3_bucket.datalake.id}"
+  acl    = "private"
+  key    = "raw/"
+  source = ""
+}
+
+resource "aws_s3_bucket_object" "dw" {
+  bucket = "${aws_s3_bucket.datalake.id}"
+  acl    = "private"
+  key    = "datawarehouse/"
   source = ""
 }
